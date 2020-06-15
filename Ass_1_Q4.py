7e331195921b8f7348bcec5d98e306ee042aa9ba
Sean Talbot <65349957+SeanNeilTalbot@users.noreply.github.com>
2020-06-15 12:27:48 +0200

    Add files via upload

---
+++ Ass_1_Q4.py
@@ -0,0 +1,11 @@
+#Displaying strings and integers with calculations
+
+nApples = 72
+nKids = 22
+
+calculation = nApples / nKids
+
+remainder = nApples%nKids
+
+print("There are", nKids, "kids and", nApples, "apples.")
+print("Each kid gets", round(calculation,0), "apples, and", remainder, "apples remain.")
