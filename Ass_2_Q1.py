b4fca1b47f8ef055d5204e9befaac00048e66f42
Sean Talbot <65349957+SeanNeilTalbot@users.noreply.github.com>
2020-06-15 12:32:55 +0200

    Add files via upload

---
+++ Ass_2_Q1.py
@@ -0,0 +1,8 @@
+#Use of IF statement and input
+
+year = int(input('Please enter the year of your birth: '))
+
+if year < 2000:
+    print(year,"- welcome from the previous century")
+else:
+    print(year,"- WELCOME")
